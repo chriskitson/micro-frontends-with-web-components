78f195644c0a81bf997e905a0185713ba4c7043b
Chris Kitson <ckitson@statestreet.com>
2019-05-08 14:59:29 +0100

    send name to react component

--- micro-fe-react/src/ExampleComponent.js
+++ micro-fe-react/src/ExampleComponent.js
@@ -1,11 +1,22 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 
 export class ExampleComponent extends React.Component {
+
+  static propTypes = {
+    name: PropTypes.string
+  }
+
+  static defaultProps = {
+    name: "CHrissy",
+  }
+
   render() {
+    const { name } = this.props;
     return (
       <div className="exampleComponent">
         <img src="/images/react.png" alt="React Logo" className="logo" />
-        <p>Hello <strong>Chris</strong> from your friendly React component.</p>
+        <p>Hello <strong>{name}</strong> from your friendly React component.</p>
       </div>
     )
   }
